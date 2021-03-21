71d124f8a39ef2d9453e5977c5bb54c119e57ca5
temirlan <ashimkhan.temirlan@gmail.com>
2021-03-21 12:17:34 +0600

    Update path

--- foi/foi/urls.py
+++ foi/foi/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(f"admin/", admin.site.urls),
-    path(f"", include("signin.urls")),
+    path(f"foi/admin/", admin.site.urls),
+    path(f"foi/", include("signin.urls")),
 ]
